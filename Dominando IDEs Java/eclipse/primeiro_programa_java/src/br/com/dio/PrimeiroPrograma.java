fc8e57616b01b6547b1a021efa3d2d73b8b016e5
annegaldino <carolinnecarvalho@gmail.com>
2023-02-23 22:25:55 -0300

    uso da ide eclipse

---
+++ Dominando IDEs Java/eclipse/primeiro_programa_java/src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,24 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Gato gato = new Gato();
+		Livro livro = new Livro();
+		System.out.println(gato);
+		System.out.println(livro);
+		
+		/*int a = 2;
+		int b = 3;
+		System.out.println("Hello Wold! " + (a+b));*/
+	}
+
+}
+
+class Livro {
+	private String nome;
+	private String nPg;
+}
