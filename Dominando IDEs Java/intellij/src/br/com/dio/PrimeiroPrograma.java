7524fa1946208bb9d2042a913635f63aff2aab11
annegaldino <carolinnecarvalho@gmail.com>
2023-02-24 15:59:46 -0300

    Criando class Livro

--- Dominando IDEs Java/intellij/src/br/com/dio/PrimeiroPrograma.java
+++ Dominando IDEs Java/intellij/src/br/com/dio/PrimeiroPrograma.java
@@ -8,9 +8,45 @@ public class PrimeiroPrograma {
 
         System.out.println(gato);
 
+        Livro livro = new Livro("o problema dos 3 corpos", "300");
+        System.out.println(livro);
         /*int a = 5;
         int b = 3;
 
         System.out.println("Hello World!");*/
+    }
+}
+
+class Livro {
+    private String nome;
+    private String nPg;
+
+    public Livro(String nome, String nPg) {
+        this.nome = nome;
+        this.nPg = nPg;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getnPg() {
+        return nPg;
+    }
+
+    public void setnPg(String nPg) {
+        this.nPg = nPg;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", nPg='" + nPg + '\'' +
+                '}';
     }
 }
